867fd4bf1882a16626e77d6874636d7248a6f316
Eugenio Thieme <eugen@thieme.it>
2017-12-06 14:31:15 +0100

    *Some preliminary graphics*
    Added some preliminary graphics in the script, now we do a faceting based on lithotype
    and distinguish between the presence and absence of water.
    -In the existing graphs we should make better labels.
    -Should compute the mean temperature.
    -Should try to invert with some semplicistic (and wrong) model.
    -Quantify the errors.
    -PCA?

--- src/radon_sce.R
+++ src/radon_sce.R
@@ -1,10 +1,51 @@
 library(ggplot2)
 library(dplyr)
 
-setwd('~/Documents/Radon')
-rn <- read.csv(file = 'misure.csv', sep=',', header = T, stringsAsFactors = F)
-#filt <- filter(rn, CID == 'LNO')
+#Setting working directory
+setwd('~/tmp/progetto_radon/')
+#Importing data file
+rn <- read.csv(file = 'data/misure.csv', sep=',', header = T, stringsAsFactors = F)
+rn <- mutate(rn, ABS_ERR = (ERR * CONC)/100)
 
-rnp <- ggplot(rn, aes(x=-DEPTH, y=CONC, color = CID)) + geom_point()
-#rnp <- rnp + facet_grid(CID ~ .)
-rnp
+
+
+#Want to check if it does exist a directory for the graphs: if not, we'll create it
+if(dir.exists('graphs') == F){
+  dir.create('graphs')
+}
+
+
+#Plotting all concentrations with depth
+rnp <- ggplot(rn, aes(x=-DEPTH, y=CONC, color = CID)) + geom_point() + 
+       labs(title='Concentration with depth', x='Depth (m)', y='Concentration (Bq/m^3)') 
+
+ggsave(rnp, file='graphs/conc.png', width = 8, height = 6)     
+
+#Now the same as above, but faceting based on cave, and adding errors as given from the lab
+rnp_fc <- ggplot(rn, aes(x=-DEPTH, y=CONC)) + geom_point() + 
+          labs(title='Concentration with depth', x='Depth (m)', y='Concentration (Bq/m^3)')  + 
+          facet_grid(. ~ CID) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
+ggsave(rnp_fc, file='graphs/conc_fc_err.png', width = 12, height = 6)   
+
+
+
+#Now the same as above, but checking for presence of water
+rnp_fc_water <- ggplot(rn, aes(x=-DEPTH, y=CONC, color=H2O)) + geom_point() + 
+  labs(title='Concentration with depth', x='Depth (m)', y='Concentration (Bq/m^3)')  + 
+  facet_grid(. ~ CID) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
+ggsave(rnp_fc_water, file='graphs/conc_fc_err.png', width = 12, height = 6)   
+
+
+#Now the same as above, but with linear smoothing
+rnp_fc_water_lm <- ggplot(rn, aes(x=-DEPTH, y=CONC, color=H2O)) + geom_point() + 
+  labs(title='Concentration with depth', x='Depth (m)', y='Concentration (Bq/m^3)')  + 
+  geom_smooth(method='lm') +
+  facet_grid(. ~ CID) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
+ggsave(rnp_fc_water_lm, file='graphs/conc_fc_err.png', width = 12, height = 6) 
+
+#Now the same as above, but with loess
+rnp_fc_water_loess <- ggplot(rn, aes(x=-DEPTH, y=CONC, color=H2O)) + geom_point() + 
+  labs(title='Concentration with depth', x='Depth (m)', y='Concentration (Bq/m^3)')  + 
+  geom_smooth(method='loess') +
+  facet_grid(. ~ CID) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
+ggsave(rnp_fc_water_loess, file='graphs/conc_fc_err.png', width = 12, height = 6) 
