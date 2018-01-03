1d522f36d697cca9c4b458000e542bafdd4d6bcf
Eugenio Thieme <eugen@thieme.it>
2018-01-03 11:15:03 +0100

    ******Modified graphs labels and dataset******
    -Graphs labels have been translated into italian.
    -Initial dataset has been slightly modified: some factors have been renamed.

--- src/radon_sce.R
+++ src/radon_sce.R
@@ -1,19 +1,35 @@
 library(ggplot2)
 library(dplyr)
+library(latex2exp)
 
 #Setting working directory
 setwd('~/tmp/progetto_radon/')
 #Importing data file
-rn <- read.csv(file = 'data/misure.csv', sep=',', header = T, stringsAsFactors = F)
+rn <- read.csv(file = 'data/misure_2.csv', sep=',', header = T, stringsAsFactors = F)
 #Computing absolute error and mean temperature
 rn <- mutate(rn, ABS_ERR = (ERR * CONC)/100)
 rn <- mutate(rn, TMEAN = (T1+T2)/2)
-
+#rn <- mutate(rn, LOG_DP = log(-DEPTH))
+#rn <- filter(rn, Grotta == 'TRM')
 #Computing correlations
 cor_conc_dep <- cor(rn$CONC, rn$DEPTH)
 cor_conc_temp <- cor(rn$CONC, rn$TMEAN)
 cor_temp_dep <- cor(rn$TMEAN, rn$DEPTH)
 
+
+# rn <- data.frame(lapply(rn, function(x) {
+#   gsub("TRM", "Tremezzo", x) 
+# }), stringsAsFactors = F)
+# 
+# rn <- data.frame(lapply(rn, function(x) {
+#   gsub("LNO", "Lino", x) 
+# }), stringsAsFactors = F)
+
+#This is for changing CID into a more explicative name
+names1 <- colnames(rn)
+names1[1] <- 'Grotta' 
+names1[5] <- 'Acqua'
+colnames(rn) <- names1
 
 #Want to check if it does exist a directory for the graphs: if not, we'll create it
 if(dir.exists('graphs') == F){
@@ -22,36 +38,43 @@ if(dir.exists('graphs') == F){
 
 
 #Plotting all concentrations with depth
-rnp <- ggplot(rn, aes(x=-DEPTH, y=CONC, color = CID)) + geom_point() + 
-       labs(title='Concentration with depth', x='Depth (m)', y='Concentration (Bq/m^3)') 
+rnp <- ggplot(rn, aes(x=-DEPTH, y=CONC, color = Grotta)) + geom_point() + 
+       labs(title='Concentrazione con la profondità', x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})')) 
 
 ggsave(rnp, file='graphs/conc.png', width = 8, height = 6)     
 
 #Now the same as above, but faceting based on cave, and adding errors as given from the lab
 rnp_fc <- ggplot(rn, aes(x=-DEPTH, y=CONC)) + geom_point() + 
-          labs(title='Concentration with depth', x='Depth (m)', y='Concentration (Bq/m^3)')  + 
-          facet_grid(. ~ CID) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
+          labs(title='Concentrazione con la profondità', x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})'))  + 
+          facet_grid(. ~ Grotta) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
 ggsave(rnp_fc, file='graphs/conc_fc_err.png', width = 12, height = 6)   
 
 
 
 #Now the same as above, but checking for presence of water
-rnp_fc_water <- ggplot(rn, aes(x=-DEPTH, y=CONC, color=H2O)) + geom_point() + 
-  labs(title='Concentration with depth', x='Depth (m)', y='Concentration (Bq/m^3)')  + 
-  facet_grid(. ~ CID) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
-ggsave(rnp_fc_water, file='graphs/conc_fc_err.png', width = 12, height = 6)   
+rnp_fc_water <- ggplot(rn, aes(x=-DEPTH, y=CONC, color=Acqua)) + geom_point() + 
+  labs(title='Concentrazione con la profondità', x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})'))  + 
+  facet_grid(. ~ Grotta) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
+ggsave(rnp_fc_water, file='graphs/conc_fc_err_water.png', width = 12, height = 6)   
 
 
 #Now the same as above, but with linear smoothing
-rnp_fc_water_lm <- ggplot(rn, aes(x=-DEPTH, y=CONC, color=H2O)) + geom_point() +
-  labs(title='Concentration with depth', x='Depth (m)', y='Concentration (Bq/m^3)')  +
+rnp_fc_water_lm <- ggplot(rn, aes(x=-DEPTH, y=CONC, color=Acqua)) + geom_point() +
+  labs(title='Concentrazione con la profondità', x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})'))  +
   geom_smooth(method='lm') +
-  facet_grid(. ~ CID) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
-ggsave(rnp_fc_water_lm, file='graphs/conc_fc_err.png', width = 12, height = 6)
+  facet_grid(. ~ Grotta) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
+ggsave(rnp_fc_water_lm, file='graphs/conc_fc_err_water_fitted.png', width = 12, height = 6)
+
+rn_filt <- filter(rn, Grotta == 'TRM')
+
+#Fitting with lm for extracting statistics
+fitted <- lm(rn_filt$CONC ~ rn_filt$DEPTH)
+summarised <- summary(fitted)
+predicted <- predict(fitted)
 
 # #Now the same as above, but with loess
-# rnp_fc_water_loess <- ggplot(rn, aes(x=-DEPTH, y=CONC, color=H2O)) + geom_point() + 
+# rnp_fc_water_loess <- ggplot(rn, aes(x=-DEPTH, y=CONC, color=Acqua)) + geom_point() + 
 #   labs(title='Concentration with depth', x='Depth (m)', y='Concentration (Bq/m^3)')  + 
 #   geom_smooth(method='loess') +
-#   facet_grid(. ~ CID) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
+#   facet_grid(. ~ Grotta) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
 # ggsave(rnp_fc_water_loess, file='graphs/conc_fc_err.png', width = 12, height = 6) 
