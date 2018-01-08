5a16f3e0cb4e5d1a41d242e52b5d39442f5fb19d
Eugenio Thieme <eugen@thieme.it>
2018-01-08 13:01:16 +0100

    **Deleted zombie code**
    Deleted some commented code: it wasn't useful anymore.

--- src/radon_sce.R
+++ src/radon_sce.R
@@ -11,19 +11,12 @@ rn <- mutate(rn, ABS_ERR = (ERR * CONC)/100)
 rn <- mutate(rn, TMEAN = (T1+T2)/2)
 #rn <- mutate(rn, LOG_DP = log(-DEPTH))
 #rn <- filter(rn, Grotta == 'TRM')
+
 #Computing correlations
 cor_conc_dep <- cor(rn$CONC, rn$DEPTH)
 cor_conc_temp <- cor(rn$CONC, rn$TMEAN)
 cor_temp_dep <- cor(rn$TMEAN, rn$DEPTH)
 
-
-# rn <- data.frame(lapply(rn, function(x) {
-#   gsub("TRM", "Tremezzo", x) 
-# }), stringsAsFactors = F)
-# 
-# rn <- data.frame(lapply(rn, function(x) {
-#   gsub("LNO", "Lino", x) 
-# }), stringsAsFactors = F)
 
 #This is for changing CID into a more explicative name
 names1 <- colnames(rn)
@@ -38,32 +31,37 @@ if(dir.exists('graphs') == F){
 
 
 #Plotting all concentrations with depth
-rnp <- ggplot(rn, aes(x=-DEPTH, y=CONC, color = Grotta)) + geom_point() + 
-       labs(title='Concentrazione con la profondità', x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})')) 
+rnp <- ggplot(rn, aes(x=DEPTH, y=CONC, color = Grotta)) + geom_point() + 
+       labs(x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})')) +
+    theme(axis.title = element_text(size=16), axis.text=element_text(size=16), legend.title = element_text(size=16),legend.text = element_text(size=16),         strip.text = element_text(size=16))
 
 ggsave(rnp, file='graphs/conc.png', width = 8, height = 6)     
 
 #Now the same as above, but faceting based on cave, and adding errors as given from the lab
-rnp_fc <- ggplot(rn, aes(x=-DEPTH, y=CONC)) + geom_point() + 
-          labs(title='Concentrazione con la profondità', x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})'))  + 
-          facet_grid(. ~ Grotta) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
+rnp_fc <- ggplot(rn, aes(x=DEPTH, y=CONC)) + geom_point() + 
+          labs(x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})'))  + 
+          facet_grid(. ~ Grotta) + geom_errorbar(aes(x =DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR))) +
+    theme(axis.title = element_text(size=16), axis.text=element_text(size=16), legend.title = element_text(size=16),legend.text = element_text(size=16),         strip.text = element_text(size=16))
 ggsave(rnp_fc, file='graphs/conc_fc_err.png', width = 12, height = 6)   
 
 
 
 #Now the same as above, but checking for presence of water
-rnp_fc_water <- ggplot(rn, aes(x=-DEPTH, y=CONC, color=Acqua)) + geom_point() + 
-  labs(title='Concentrazione con la profondità', x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})'))  + 
-  facet_grid(. ~ Grotta) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
+rnp_fc_water <- ggplot(rn, aes(x=DEPTH, y=CONC, color=Acqua)) + geom_point() + 
+  labs(x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})'))  + 
+  facet_grid(. ~ Grotta) + geom_errorbar(aes(x =DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR))) +
+    theme(axis.title = element_text(size=16), axis.text=element_text(size=16), legend.title = element_text(size=16),legend.text = element_text(size=16),         strip.text = element_text(size=16))
 ggsave(rnp_fc_water, file='graphs/conc_fc_err_water.png', width = 12, height = 6)   
 
 
 #Now the same as above, but with linear smoothing
-rnp_fc_water_lm <- ggplot(rn, aes(x=-DEPTH, y=CONC, color=Acqua)) + geom_point() +
-  labs(title='Concentrazione con la profondità', x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})'))  +
+rnp_fc_water_lm <- ggplot(rn, aes(x=DEPTH, y=CONC, color=Acqua)) + geom_point() +
+  labs(x='Profondità (m)', y=TeX('Concentrazione ($Bq/m^3$)})'))  +
   geom_smooth(method='lm') +
-  facet_grid(. ~ Grotta) + geom_errorbar(aes(x =-DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR)))
-ggsave(rnp_fc_water_lm, file='graphs/conc_fc_err_water_fitted.png', width = 12, height = 6)
+  facet_grid(. ~ Grotta) + geom_errorbar(aes(x =DEPTH, ymin = (CONC - ABS_ERR), ymax = (CONC + ABS_ERR))) +
+  theme(axis.title = element_text(size=16), axis.text=element_text(size=16), legend.title = element_text(size=16),legend.text = element_text(size=16),
+        strip.text = element_text(size=16))
+ggsave(rnp_fc_water_lm, file='graphs/conc_fc_err_water_fitted.png', width = 12, height = 6) 
 
 rn_filt <- filter(rn, Grotta == 'TRM')
 
